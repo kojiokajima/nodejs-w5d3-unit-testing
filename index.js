cd6cf7ad3d0f52a858e2e00c5bf4961420b390f2
Koji Okajima <koji970124@gmail.com>
2021-06-02 11:13:47 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express')
+
+const utils = require('./util/task-schema')
+
+const app = express()
+
+app.use(express.json())
+
+const tasks = [
+  { id: 1, name: "Task 1", completed: true},
+  { id: 2, name: "Task 2", completed: true},
+  { id: 3, name: "Task 3", completed: false},
+]
+
+// GET
+app.get('/api/tasks', (req, res) => {
+  res.send(tasks)
+})
+
+// GET (BY ID)
+app.get('/api/task/:id', (req, res) => {
+  const taskId = req.params.id
+  const task = tasks.find(task => task.id === +taskId)
+  if(!task) return res.status(404).send('The task with the provided ID does not exist')
+  res.send(task)
+})
+
+// POST
+app.post('/api/tasks', (req, res) => {
+  // validation check
+  const { error } = utils.validateTask(req.body)
+
+  if(error) return res.status(400).send('The name should ve at least 3 characters long')
+
+  const task = {
+    id: tasks.length + 1,
+    name: req.body.name,
+    completed: req.body.completed
+  }
+
+  tasks.push(task)
+  res.status(201).send(task)
+})
+
+// PUT
+app.put('/api/tasks/:id', (req, res) => {
+  const taskId = req.params.id
+  const task = tasks.find(task => task.id === +taskId) 
+  if(!task) return res.status(404).send("The task with the provided ID does not exist")
+
+  const { error } = utils.validateTask(req.body)
+
+  if(error) return res.status(400).send('The name should ve at least 3 characters long')
+
+  task.name = req.body.name
+  task.completed = req.body.completed
+
+  res.send(task)
+
+})
+
+// PATCH
+app.patch('/api/tasks/:id', (req, res) => {
+  const taskId = req.params.id
+  const task = tasks.find(task => task.id === +taskId) 
+  if(!task) return res.status(404).send("The task with the provided ID does not exist")
+
+  const { error } = utils.validateTask(req.body)
+
+  if(error) return res.status(400).send('The name should ve at least 3 characters long')
+
+  task.name = req.body.name
+  if(req.body.completed){
+    task.completed = req.body.completed
+  }
+  res.send(task)
+})
+
+// DELETE
+app.delete('/api/tasks/:id', (req, res) => {
+  const taskId = req.params.id
+  const task = tasks.find(task => task.id === +taskId) 
+  if(!task) return res.status(404).send("The task with the provided ID does not exist")
+
+  const index = tasks.indexOf(task)
+  tasks.splice(index, 1)
+  res.send(tasks)
+})
+
+const PORT = process.env.PORT || 8080
+
+module.exports = app.listen(PORT, () => console.log(`Server listening on PORT: ${PORT}`))
---
+++ test/task.js
@@ -0,0 +1,162 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const { response } = require('express')
+const server = require('../index')
+
+// specify assertation style
+chai.should()
+
+// enable to call our RESTful API
+chai.use(chaiHttp)
+
+describe('GET ALL /api/task', function() {
+  it('should GET all the tasks', function(done) {
+    chai.request(server).get('/api/tasks').end((err, response) => {
+      response.should.have.status(200)
+      response.body.should.be.a('array')
+      response.body.length.should.be.eq(3)
+    })
+    done()
+  })
+
+  it('should NOT GET all the tasks', function(done){
+    chai.request(server).get('/api/task').end((err, response) => {
+      response.should.have.status(404)
+    })
+    done()
+  })
+})
+
+describe('GET task by ID', () => {
+  it('should GET a task by ID', (done) => {
+    const taskId = 1
+    chai.request(server).get('/api/task/' + taskId).end((err, response) => {
+      response.should.have.status(200)
+      response.body.should.be.a('object')
+      response.body.should.have.property('id')
+      response.body.should.have.property('name')
+      response.body.should.have.property('completed')
+      response.body.should.have.property('id').eq(1)
+    })
+    done()
+  })
+
+  it('should NOT GET a task by ID', (done) => {
+    const taskId = 123
+    console.log('/api/task'+taskId);
+    chai.request(server).get('/api/task/' + taskId).end((err, response) => {
+      response.should.have.status(404)
+      response.text.should.be.eq('The task with the provided ID does not exist')
+    })
+    done()
+  })
+})
+
+describe('POST /api/tasks', () => {
+  it('should POST a new task', (done) => {
+    const task = {
+      name: 'Task 4',
+      completed: false
+    }
+    chai.request(server).post('/api/tasks').send(task).end((err, response) => {
+      response.should.have.status(201)
+      response.body.should.be.a('object')
+      response.body.should.have.property('id').eq(4)
+      response.body.should.have.property('name').eq("Task 4")
+      response.body.should.have.property('completed').eq(false)
+    })
+    done()
+  })
+
+  it('should NOT POST a new task without the name property', (done) => {
+    const task = {
+      name: '',
+      completed: false
+    }
+
+    chai.request(server).post('/api/tasks').send(task).end((err, response) => {
+      response.should.have.status(400)
+      response.text.should.be.eq('The name should ve at least 3 characters long')
+    })
+    done()
+  })
+})
+
+
+describe('PUT /api/tasks/:id', () => {
+  it('should PUT an existing task', (done) => {
+    const taskId = 1
+    const task = {
+      name: "Task 1 changed",
+      completed: true
+    }
+    chai.request(server).put('/api/tasks/' + taskId).send(task).end((err, response) => {
+      response.should.have.status(200)
+      response.body.should.be.a('object')
+      response.body.should.have.property('id').eq(1)
+      response.body.should.have.property('name').eq('Task 1 changed')
+      response.body.should.have.property('completed').eq(true)
+    })
+    done()
+  })
+
+  it('should NOT PUT an existing task with a name that is less than 3 characters', (done) => {
+    const taskId = 1
+    const task = {
+      name: "Ta",
+      completed: true
+    }
+    chai.request(server).put('/api/tasks/' + taskId).send(task).end((err, response) => {
+      response.should.have.status(400)
+      response.text.should.be.eq('The name should ve at least 3 characters long')
+    })
+    done()
+  })
+})
+
+describe('PATCH /api/tasks/:id', () => {
+  it('should PATCH an existing tasks', (done) => {
+    const taskId = 1
+    const task = {
+      name: 'Task 1 patch'
+    }
+    chai.request(server).patch('/api/tasks/' + taskId).send(task).end((err, response) => {
+      response.should.have.status(200)
+      response.body.should.be.a('object')
+      response.body.should.have.property('id').eq(1)
+      response.body.should.have.property('name').eq('Task 1 patch')
+    })
+    done()
+  })
+
+  it('should NOT PATCH an existing tasks with a name that is less than 3 characters long', (done) => {
+    const taskId = 1
+    const task = {
+      name: "T",
+    }
+    chai.request(server).patch('/api/tasks/' + taskId).send(task).end((err, response) => {
+      response.should.have.status(400)
+      response.text.should.be.eq('The name should ve at least 3 characters long')
+    })
+    done()
+  })
+})
+
+describe('DELETE /api/tasks/:id', () => {
+  it('should DELETE an existing task', (done) => {
+    const taskId = 1
+    chai.request(server).delete('/api/tasks/' + taskId).end((err, response) => {
+      response.should.have.status(200)
+      done()
+    })
+  })
+
+  it('should NOT DELETE a task that is not in the database', (done) => {
+    const taskId = 45
+    chai.request(server).delete('/api/tasks/' + taskId).end((err, response) => {
+      response.should.have.status(404)
+      response.text.should.be.eq("The task with the provided ID does not exist")
+      done()
+    })
+  })
+})
